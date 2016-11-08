207599b501c55377017efc7cbce853f4784a1da1
Julka Grodel <julka@addthis.com>
2016-11-08 16:44:42 -0500

    T66058 adding addthis_plugin_info for tracking purposes

--- src/jquery-addthis.js
+++ src/jquery-addthis.js
@@ -6,5 +6,17 @@
 */
 (function($, window) {
     var pluginName = 'addthis';
-    var plugin = $[pluginName] = {};
+    var plugin = $.fn[pluginName] = {};
+
+    // Variable for tracking module usage to help guide AddThis in deciding how
+    // many resources to devote to maintaining this integration and what
+    // versions of jQuery to focus on or test with.
+    window.addthis_plugin_info = {
+        info_status    : 'enabled',
+        cms_name       : 'jQuery',
+        cms_version    : $.fn.jquery,
+        plugin_name    : 'jquery-angular',
+        plugin_version : '0.0.1',
+        plugin_mode    : 'AddThis'
+    };
 })(jQuery, window);
