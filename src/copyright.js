7ed3b288dbaf8327ee87f63ee9e0fa5e29a4d40f
Julka Grodel <julka@addthis.com>
2016-11-08 15:14:12 -0500

    T66058 initial commit

---
+++ src/copyright.js
@@ -0,0 +1,5 @@
+/**
+ * @license jquery-addthis 0.0.1
+ * Copyright (c) 2016 Oracle and/or its affiliates.
+ * License: MIT
+ */
---
+++ src/jquery-addthis.js
@@ -0,0 +1,10 @@
+/**
+* @file A jQuery plugin for adding AddThis tools onto dynamically generate pages. Requires a free AddThis account.
+* @author AddThis <help@addthis.com>
+* @version 0.0.1
+* @requires jquery
+*/
+(function($, window) {
+    var pluginName = 'addthis';
+    var plugin = $[pluginName] = {};
+})(jQuery, window);
