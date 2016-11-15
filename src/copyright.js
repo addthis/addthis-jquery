b4e123238fb6f912519559dad51c983e81d71347
Julka Grodel <julka@addthis.com>
2016-11-15 17:55:20 -0500

    copyright jdsoc tags

--- src/copyright.js
+++ src/copyright.js
@@ -1,5 +1,5 @@
 /**
- * @license jquery-addthis 0.0.1
- * Copyright (c) 2016 Oracle and/or its affiliates.
- * License: MIT
+ * jquery-addthis 0.0.1
+ * @copyright 2016 Oracle and/or its affiliates.
+ * @license MIT
  */
